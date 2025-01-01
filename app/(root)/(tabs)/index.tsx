1528b11b75f406f17b41ddaa7fbbaa5f6b19c674
Aarya Chandak <aaryachandak02@gmail.com>
2025-01-01 11:47:22 -0600

    main

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className= "font-bold text-lg my-10"> Welcome dumbass</Text>
+      <Text className= "font-bold text-lg my-10 font-rubik text-3xl"> Welcome dumbass</Text>
       <Link href="/sign-in">Sign-in</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
