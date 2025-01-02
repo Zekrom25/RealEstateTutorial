06c2033f7b87aa09556236a4447060b94cb9db2f
Aarya Chandak <aaryachandak02@gmail.com>
2025-01-01 20:36:46 -0600

    Started work on the profile page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,13 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className= "font-bold text-lg my-10 font-rubik text-3xl"> Welcome dumbass</Text>
-      <Link href="/sign-in">Sign-in</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href={{ pathname: "/properties/[id]", params: { id: "1" } }}>
-        Properties
-      </Link>
+      <Text className= "font-bold text-lg my-10 font-rubik text-2xl"> Welcome to Restate</Text>
+
     </View>
   );
 }
--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import icons from '@/constants/icons'
 
 const profile = () => {
+  const handleLogout = async() => {}
+
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
+    <SafeAreaView className ="h-full bg-white">
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerClassName="pb-32 px-7">
+        <View className = "flex flex-row items-center justify-between mt-5">
+          <Text className = "text-xl font-rubik-bold">Profile</Text>
+          <Image className = "size-5" source = {icons.bell}/>
+        </View>
+
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
