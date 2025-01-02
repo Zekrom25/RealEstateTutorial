56e8c94c22f35590eafa3fb351746e73f29440bb
Aarya Chandak <aaryachandak02@gmail.com>
2025-01-01 19:29:58 -0600

    working auth login

---
+++ lib/global-provider.tsx
@@ -0,0 +1,48 @@
+import {createContext, ReactNode, useContext} from "react";
+import { useAppwrite } from "./useAppwrite";
+import {getCurrentUser} from "./appwrite"
+
+interface User {
+    $id: string;
+    name:string;
+    email: string;
+    avatar: string;
+}
+
+interface GlobalContextType {
+    isLoggedIn: boolean;
+    user: User | null;
+    loading: boolean;
+    refetch: (newParams?: Record<string, string| number>) => Promise<void>;
+}
+
+const GlobalContext = createContext<GlobalContextType | undefined>(undefined);
+
+export const GlobalProvider = ({children} : {children: ReactNode}) => {
+
+    const {data: user, loading, refetch} = useAppwrite( {fn: getCurrentUser})
+
+    const isLoggedIn = !!user;
+
+    console.log(JSON.stringify(user,null,2))
+
+    return(
+        <GlobalContext.Provider value={{
+            isLoggedIn, user, loading,refetch,
+        }}>
+            {children}
+        </GlobalContext.Provider>
+
+    )
+}
+
+export const useGlobalContext = (): GlobalContextType => {
+    const context = useContext(GlobalContext)
+
+    if(!context) {
+        throw new  Error('must be used within a global provider')
+    }
+    return context;
+}
+
+export default GlobalProvider;
